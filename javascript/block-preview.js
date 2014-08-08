d0bd8f85c4c5e7bb284d5d4cfe0c1ee908b87225
Shea Dawson <shea@livesource.co.nz>
2014-08-08 11:05:04 +1000

    Fixed js errors with preview

--- javascript/block-preview.js
+++ javascript/block-preview.js
@@ -1,15 +1,11 @@
-jQuery.noConflict();
-
 (function($){
 
-    $(document).ready(function(){
-
+	$(function(){
  		$('.block-area').each(function(){
  			var self = $(this);
  			var label = '<span>' + self.data('areaid') + ' Block Area</span>';
  			self.append('<div class="block-preview-overlay"></div><div class="block-preview-label">' + label + '</div>');
-
  		});
-    });
+	});
  
 }(jQuery));
