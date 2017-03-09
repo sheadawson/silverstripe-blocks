c1327f7e44135b5db91605738a6c95f52b71c07b
Jack O'Connor <jack.oconnor@quadradigital.co.uk>
2017-03-09 16:47:38 +0000

    Fixing missing use cases and broken array

--- src/tasks/BlockUpgradeTask.php
+++ src/tasks/BlockUpgradeTask.php
@@ -6,6 +6,7 @@ use SheaDawson\Blocks\Model\Block;
 use SheaDawson\Blocks\Model\BlockSet;
 use SilverStripe\ORM\DB;
 use SilverStripe\SiteConfig\SiteConfig;
+use SilverStripe\Dev\BuildTask;
 
 /**
  * BlockUpgradeTask
@@ -15,61 +16,61 @@ use SilverStripe\SiteConfig\SiteConfig;
  */
 class BlockUpgradeTask extends BuildTask
 {
-    public function run($request)
-    {
+	public function run($request)
+	{
 
-        // update block/set titles
-        // Name field has been reverted back to Title
-        // DB::query("update Block set Name = Title");
-        // DB::query("update BlockSet set Name = Title");
+		// update block/set titles
+		// Name field has been reverted back to Title
+		// DB::query("update Block set Name = Title");
+		// DB::query("update BlockSet set Name = Title");
 
-        // update block areas
+		// update block areas
 
-        DB::query('
+		DB::query('
 			update SiteTree_Blocks
 			left join Block on SiteTree_Blocks.BlockID = Block.ID
 			set BlockArea = Block.Area
 			where BlockID = Block.ID
 		');
 
-        // update block sort
+		// update block sort
 
-        DB::query('
+		DB::query('
 			update SiteTree_Blocks
 			left join Block on SiteTree_Blocks.BlockID = Block.ID
 			set Sort = Block.Weight
 			where BlockID = Block.ID
 		');
 
-        echo 'BlockAreas, Sort updated<br />';
+		echo 'BlockAreas, Sort updated<br />';
 
-        // migrate global blocks
+		// migrate global blocks
 
-        $sc = SiteConfig::current_site_config();
-        if ($sc->Blocks()->Count()) {
-            $set = BlockSet::get()->filter('Title', 'Global')->first();
-            if (!$set) {
-                $set = BlockSet::create(
-                    'Title' => 'Global',
-                ]);
-                $set->write();
-            }
-            foreach ($sc->Blocks() as $block) {
-                if (!$set->Blocks()->find('ID', $block->ID)) {
-                    $set->Blocks()->add($block, [
-                        'Sort' => $block->Weight,
-                        'BlockArea' => $block->Area,
-                    ]);
-                    echo "Block #$block->ID added to Global block set<br />";
-                }
-            }
-        }
+		$sc = SiteConfig::current_site_config();
+		if ($sc->Blocks()->Count()) {
+			$set = BlockSet::get()->filter('Title', 'Global')->first();
+			if (!$set) {
+				$set = BlockSet::create([
+					'Title' => 'Global',
+				]);
+				$set->write();
+			}
+			foreach ($sc->Blocks() as $block) {
+				if (!$set->Blocks()->find('ID', $block->ID)) {
+					$set->Blocks()->add($block, [
+						'Sort' => $block->Weight,
+						'BlockArea' => $block->Area,
+					]);
+					echo "Block #$block->ID added to Global block set<br />";
+				}
+			}
+		}
 
-        // publish blocks
-        $blocks = Block::get()->filter('Published', 1);
-        foreach ($blocks as $block) {
-            $block->publish('Stage', 'Live');
-            echo "Published Block #$block->ID<br />";
-        }
-    }
+		// publish blocks
+		$blocks = Block::get()->filter('Published', 1);
+		foreach ($blocks as $block) {
+			$block->publish('Stage', 'Live');
+			echo "Published Block #$block->ID<br />";
+		}
+	}
 }
