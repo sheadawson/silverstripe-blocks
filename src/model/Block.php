e194b50a32c2925806dc53e186740fc25466c293
Chris Lock <chris.l@biffbangpow.com>
2017-10-03 11:05:45 +0100

    Adding a config var so developers can define a defaultBlockArea

--- src/model/Block.php
+++ src/model/Block.php
@@ -366,6 +366,16 @@ class Block extends DataObject implements PermissionProvider
         ];
     }
 
+		public function onBeforeWrite()
+		{
+				$defaultBlockArea = Block::config()->get('defaultBlockArea');
+				$blockArea = $this->getField('ManyMany[BlockArea]');
+				if ($defaultBlockArea !== false && $blockArea === null) {
+						$this->setField('ManyMany[BlockArea]', $defaultBlockArea);
+				}
+				parent::onBeforeWrite();
+		}
+
     public function onAfterWrite()
     {
         parent::onAfterWrite();
