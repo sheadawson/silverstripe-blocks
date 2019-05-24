a8d84955dc4338e7314366cb10dffa1e61c9a7ea
EK1771 <smithm@osolia.com>
2019-05-24 16:26:04 +1000

    Update _config.php

--- _config.php
+++ _config.php
@@ -4,5 +4,7 @@ if (!defined('BLOCKS_DIR')) {
     define('BLOCKS_DIR', rtrim(basename(dirname(__FILE__))));
 }
 
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
+
 Config::inst()->update('LeftAndMain', 'extra_requirements_javascript', array(BLOCKS_DIR.'/javascript/blocks-cms.js'));
 Config::inst()->update('BlockAdmin', 'menu_icon', BLOCKS_DIR.'/images/blocks.png');
